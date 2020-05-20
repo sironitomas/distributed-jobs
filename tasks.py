c10700eb2f5269ec202ac98db06700b5681bd63b
Tomás Sironi <sironitomas@gmail.com>
2020-05-20 12:19:36 -0300

    Adds count_system_processes() function

--- tasks.py
+++ tasks.py
@@ -1,5 +1,7 @@
-from celery import Celery
 import time
+from subprocess import PIPE, Popen
+
+from celery import Celery
 
 app = Celery()
 app.config_from_object('celeryconfig')
@@ -10,3 +12,12 @@ def add(x, y):
     # simulate lengthy computational task
     time.sleep(15)
     return x + y
+
+
+@app.task
+def count_system_processes():
+    p1 = Popen(['ps', '-aux'], stdout=PIPE)
+    p2 = Popen(['wc', '-l'], stdin=p1.stdout, stdout=PIPE)
+    p1.stdout.close()
+    process_count = p2.communicate()[0].decode().strip()
+    print('There are {} running processes in the system'.format(process_count))
