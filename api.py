f4e34c59ebc1e85cbb883e5e7adfc2c65db22d1c
Tomás Sironi <sironitomas@gmail.com>
2020-05-21 15:18:10 -0300

    Adds /status/active endpoint

--- api.py
+++ api.py
@@ -1,3 +1,4 @@
+from celery.app.control import Inspect
 from celery.result import AsyncResult
 from flask import Flask, jsonify, request
 from markupsafe import escape
@@ -5,11 +6,6 @@ from markupsafe import escape
 from tasks import add as celery_add
 
 app = Flask(__name__)
-
-
-@app.route('/')
-def index():
-    return 'Index'
 
 
 @app.route('/task/add', methods=['POST'])
@@ -21,6 +17,13 @@ def add():
 
 
 @app.route('/status/<uuid:id>', methods=['GET'])
-def status(id):
+def get_status(id):
     result = AsyncResult(id)
     return jsonify(status=result.status)
+
+
+@app.route('/status/active', methods=['GET'])
+def get_active():
+    i = Inspect()
+    active_tasks = i.active()
+    return jsonify(active_tasks)
